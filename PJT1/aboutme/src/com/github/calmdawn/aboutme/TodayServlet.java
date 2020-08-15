8cf1db3f8d9fd8ab05c39b0c8670bdf0ae2b65ee
CalmDawn <qnfqus@gmail.com>
2020-08-16 05:56:12 +0900

    move file to Dynamic Web Project folder + add Servlet

---
+++ PJT1/aboutme/src/com/github/calmdawn/aboutme/TodayServlet.java
@@ -0,0 +1,62 @@
+package com.github.calmdawn.aboutme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/TodayServlet")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		LocalDateTime dateTime = LocalDateTime.now();
+		String nowTime = "현재시간 : " + dateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+		
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+	
+		out.print("<a href = http://localhost:8080/aboutme/index.html>메인화면</a>");
+	
+		
+		out.print("<h1>"+nowTime+"</h1>");
+		
+		
+
+		out.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
